06121e72f7cb9e9fc954723bdc552da14fddd889
variablemayank <mayank202020@rediffmail.com>
2018-10-04 17:58:08 +0530

    f

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
 const app = express();
 //DB config 
 
@@ -16,6 +20,12 @@ mongoose
 
 
 app.get('/',(req,res) => res.send("Hello hh"));
+
+// Use Routes
+app.use('/api/users',users);
+app.use('/api/profile',profile);
+app.use('/api/posts',posts);
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
