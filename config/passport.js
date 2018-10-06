37ab341ad3d85f151847e18e1b258c1fa616df95
variablemayank <mayank202020@rediffmail.com>
2018-10-06 23:40:25 +0530

    jwt authentication strategy

--- config/passport.js
+++ config/passport.js
@@ -4,13 +4,21 @@ const mongoose = require('mongoose');
 const User = mongoose.model('users');
 const keys = require('../config/keys');
 
+
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = keys.secretOrkey;
 
 module.exports = passport => {
     passport.use(new JWTStrategy(opts, (jwt_payload,done) => {
-    console.log(jwt_payload);
+    User.findById(jwt_payload.id)
+        .then(user =>{
+            if(user){
+                return done(null,user);
+            }
+            return done(null,false);
+        })
+        .catch(err => console.log(err));
     })
     );
 }
